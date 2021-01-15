cd700f588dde05b658ec301ee1896fcb6e7c4e2f
budzich <m.s.budzich@gmail.com>
2021-01-15 11:58:42 +0300

    added post request

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ const validEmail = /\S+@\S+\.\S+/;
 const validUsername = /^[a-zA-Z0-9]+$/;
 let validForm = true;
 
+const User = (name, email, password) => (
+  {
+    username: name,
+    email: email,
+    password: password,
+  }
+)
+
 const passwordCheck = () => {
   validForm = validForm && (passwordForm.value === confirmForm.value) && (passwordForm.value.match(validPassword));
 }
@@ -22,13 +30,29 @@ const emailCheck = () => {
   validForm = validForm && emailForm.value.match(validEmail);
 }
 
-form.addEventListener('submit', (e) => {
-  passwordCheck();
-  usernameCheck();
-  emailCheck();
-  if ((!validForm || !checkForm.checked)) {
-    console.log(new Error('Incorrect'));
+form.addEventListener('submit', async (e) => {
+  try {
     e.preventDefault();
-    validForm = true;
+    passwordCheck();
+    usernameCheck();
+    emailCheck();
+    if ((!validForm || !checkForm.checked)) {
+      console.log(new Error('Incorrect'));
+      validForm = true;
+    } else {
+      const response = await fetch('https://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(User(usernameForm.value, emailForm.value, passwordForm.value))
+      });
+      const {data} = await response.json();
+      const userdata = JSON.parse(data);
+
+      document.body.insertAdjacentHTML('beforeend', `<p>Hello, ${userdata['username']}</p>`);
+    }
+  } catch (err) {
+    throw new Error(err);
   }
 });
